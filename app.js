39609682df19f553e0094c2bc79761bec22cddd3
gheorghinca <gheorghinca.vlad@gmail.com>
2017-11-19 21:50:37 +0100

    ses

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(path.join(__dirname + '/build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 const port = process.env.PORT || 8080;
